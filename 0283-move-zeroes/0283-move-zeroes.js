140054fdd1fead6eec6f305993cf04c89b86bd63
Jason Dai <53287158+jasond20@users.noreply.github.com>
2024-03-31 12:33:57 -0400

    Time: 66 ms (92.50%), Space: 53.5 MB (85.71%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -3,13 +3,12 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-    let nonZeroFoundAt = 0;
-    for (let i = 0; i < nums.length; i++){
-        if (nums[i] != 0){
-            nums[nonZeroFoundAt++] = nums[i];
-        }
+    for (let cur = 0, lastNonZeroFoundAt = 0; cur < nums.length; cur++){
+        if (nums[cur] != 0){
+            let temp = nums[lastNonZeroFoundAt];
+            nums[lastNonZeroFoundAt++] = nums[cur];
+            nums[cur] = temp;
+        }   
     }
-    for (let i = nonZeroFoundAt; i < nums.length; i++){
-        nums[i] = 0;
-    }
+        
 };
