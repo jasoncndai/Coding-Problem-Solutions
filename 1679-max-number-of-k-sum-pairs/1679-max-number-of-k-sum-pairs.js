ccbbacda3bdeea764f3005a39a15618bfab45851
Jason Dai <53287158+jasond20@users.noreply.github.com>
2024-04-02 16:53:27 -0400

    Time: 134 ms (69.06%), Space: 60.3 MB (78.80%) - LeetHub

--- 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
+++ 1679-max-number-of-k-sum-pairs/1679-max-number-of-k-sum-pairs.js
@@ -4,15 +4,24 @@
  * @return {number}
  */
 var maxOperations = function(nums, k) {
-    // Using a map
-    let map = {};
+    // sort array, two pointers one at start and one at end
+    // if sum is less than k, move start pointer to right
+    // if sum is greater than k, move end pointer to left
+    // if sum is equal to k, increment count and move both pointers
+    nums.sort((a, b) => a - b);
+    let start = 0;
+    let end = nums.length - 1;
     let count = 0;
-    for (let i = 0; i < nums.length; i++) {
-        if (map[k - nums[i]] > 0) {
+    while (start < end) {
+        const sum = nums[start] + nums[end];
+        if (sum === k) {
             count++;
-            map[k - nums[i]]--;
+            start++;
+            end--;
+        } else if (sum < k) {
+            start++;
         } else {
-            map[nums[i]] = (map[nums[i]] || 0) + 1;
+            end--;
         }
     }
     return count;
