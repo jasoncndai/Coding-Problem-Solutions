e803899c92316722f9ec0f912fbc8de2d62576f8
Jason Dai <53287158+jasond20@users.noreply.github.com>
2024-04-02 18:09:26 -0400

    Time: 55 ms (99.42%), Space: 51 MB (99.47%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.js
@@ -7,21 +7,17 @@ var maxVowels = function(s, k) {
     // Similar to Max Avg Subarray, initialize window and then slide window over
     var sum = 0;
     var vowels = "aeiou";
-    for (var j of s.substring(0,k)){
-        if (vowels.includes(j)){
-            sum++;
-        }
-    }
     var max = sum;
-    for (var i = k; i < s.length; i++) {
+    for (var i = 0; i < s.length; i++) {
         // If next character is vowel, add 1 to sum
         if (vowels.includes(s[i])){
             sum++;
         }
         // if previous character no long in window is vowel, subtract 1 from sum
-        if (vowels.includes(s[i-k])){
+        if (i >= k && vowels.includes(s[i-k])){
             sum--;
         }
+        
         // compare current substring vowels with max vowel substring
         max = Math.max(max, sum);
     }
