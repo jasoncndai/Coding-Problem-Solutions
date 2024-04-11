4b9d96bab3c90912658d60636fca55c9c41415a2
Jason Dai <53287158+jasond20@users.noreply.github.com>
2024-04-11 14:02:22 -0400

    Time: 36 ms (66.99%), Space: 17.7 MB (33.96%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -10,10 +10,10 @@ class Solution:
         if not head or not head.next:
             return head
         # head recursion, start at the end of the linked list and work backwards
-        p = self.reverseList(head.next)
+        newHead = self.reverseList(head.next)
         # head.next.next refers to pointer pointing to last node in linked list due to base case returning head = end
         # head.next.next is the pointer from last node to None, point it back to current node that is second last
         head.next.next = head
         # removes the old pointer original pointer before returning new head from last node to second last node
         head.next = None
-        return p
+        return newHead
